56752cf63b189a2980d02b66517722041aeb05d1
Filipemtb <filipera3@gmail.com>
2023-03-23 17:15:16 -0300

    create home page

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import GlobalSyles from './styles/global';
 
 import theme from './styles/theme';
 
-import { Details }  from './pages/Details';
+import { Home }  from './pages/Home';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
     <GlobalSyles />
-    <Details />
+    <Home />
     </ThemeProvider>
   </React.StrictMode>,
 )
