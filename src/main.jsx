9de7458c7911d5b3c3c6dc29acd8648f4aa87fe7
Filipemtb <filipera3@gmail.com>
2023-03-24 15:31:13 -0300

    crate signIn page

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import GlobalSyles from './styles/global';
 
 import theme from './styles/theme';
 
-import { Home }  from './pages/Home';
+import { SignIn }  from './pages/SignIn';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
     <GlobalSyles />
-    <Home />
+    <SignIn />
     </ThemeProvider>
   </React.StrictMode>,
 )
