b55b9ab09307a92ddd41750a45f463440362116a
Filipemtb <filipera3@gmail.com>
2023-03-27 15:11:38 -0300

    create newNote page

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import GlobalSyles from './styles/global';
 
 import theme from './styles/theme';
 
-import { Profile }  from './pages/Profile';
+import { New }  from './pages/New';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
     <GlobalSyles />
-    <Profile />
+    <New />
     </ThemeProvider>
   </React.StrictMode>,
 )
